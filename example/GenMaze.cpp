db34a3b33379739b232296d6bb536f5ebb3abf7a
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-11-06 18:29:38 +0800

    update example

--- example/GenMaze.cpp
+++ example/GenMaze.cpp
@@ -1,12 +1,15 @@
 #include "../src/MazeImage++.h"
 
-int main(){
+int main(void){
+    // Small maze
     mazeimg_library::MazeImgGenerator gen_maze(M_SIZE::s);
     gen_maze.processImage("small_maze", IMG_FORMAT::PNG);
 
+    // Medium maze
     gen_maze.setMazeSize(M_SIZE::m);
     gen_maze.processImage("medium_maze", IMG_FORMAT::PNG);
 
+    // Large maze
     gen_maze.setMazeSize(M_SIZE::l);
     gen_maze.processImage("large_maze", IMG_FORMAT::PNG);
 
