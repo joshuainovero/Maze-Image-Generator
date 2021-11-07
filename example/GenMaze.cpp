342fb3c76644b6e68b2c2b1040285aca1c7ac6d0
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-11-08 01:36:27 +0800

    update example

--- example/GenMaze.cpp
+++ example/GenMaze.cpp
@@ -1,17 +1,21 @@
-#include "../src/MazeImage++.h"
+#include <MazeImage++/MazeImage++.h>
 
 int main(void){
-    // Small maze
     mazeimg_library::MazeImgGenerator gen_maze(M_SIZE::s);
-    gen_maze.processImage("small_maze", IMG_FORMAT::PNG);
+
+    // enable solution - display shortest path
+    gen_maze.enableSolution(true);
+
+    // Small maze
+    gen_maze.generate("small_maze", IMG_FORMAT::PNG);
 
     // Medium maze
     gen_maze.setMazeSize(M_SIZE::m);
-    gen_maze.processImage("medium_maze", IMG_FORMAT::PNG);
+    gen_maze.generate("medium_maze", IMG_FORMAT::PNG);
 
     // Large maze
     gen_maze.setMazeSize(M_SIZE::l);
-    gen_maze.processImage("large_maze", IMG_FORMAT::PNG);
+    gen_maze.generate("large_maze", IMG_FORMAT::PNG);
 
     return 0;
 }
